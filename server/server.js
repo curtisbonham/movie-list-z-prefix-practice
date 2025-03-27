1eaba9f6237a088d363cbfb75ca7f91d61952996
curtisbonham <curtis.bonham1@gmail.com>
2025-03-27 09:15:07 -0600

    first commit, server running

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
